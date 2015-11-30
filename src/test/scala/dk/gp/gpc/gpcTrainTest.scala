1c44c905ccee2b355d6024c9cca4018f6121706e
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2015-11-30 12:16:34 +0000

    Implementing gpc.

--- src/test/scala/dk/gp/gpc/gpcTrainTest.scala
+++ src/test/scala/dk/gp/gpc/gpcTrainTest.scala
@@ -26,16 +26,15 @@ class gpcTrainTest {
     val model = GpcModel(x, y, covFunc, covFuncParams, mean)
     val trainedModel = gpcTrain(model, maxIter = 10)
 
-    assertEquals(-1.5206, trainedModel.mean, 0.0001)
+    assertEquals(-1.3039721, trainedModel.mean, 0.0001)
 
-    assertEquals(3.6588, trainedModel.covFuncParams(0), 0.0001) //logSf
-    assertEquals(0.5046, trainedModel.covFuncParams(1), 0.0001) //logEllx1
-    assertEquals(1.35238, trainedModel.covFuncParams(2), 0.0001) //logEllx2
+    assertEquals(3.760097712540231, trainedModel.covFuncParams(0), 0.0001) //logSf
+    assertEquals(0.607193369065617, trainedModel.covFuncParams(1), 0.0001) //logEllx1
+    assertEquals(1.486723223177382, trainedModel.covFuncParams(2), 0.0001) //logEllx2
 
     val predicted = gpcPredict(t, trainedModel)
-
-    assertEquals(0.2595, predicted(6480), 0.0001) // t = [4 -4]
-    assertEquals(0.32380, predicted(1255), 0.0001) //t = [-2.5 0]
-    assertEquals(0.91336, predicted(1242), 0.0001) //t = [-2.5 -1.3]
+    assertEquals(0.227861, predicted(6480), 0.0001) // t = [4 -4]
+    assertEquals(0.291227, predicted(1255), 0.0001) //t = [-2.5 0]
+    assertEquals(0.900932, predicted(1242), 0.0001) //t = [-2.5 -1.3]
   }
 }
